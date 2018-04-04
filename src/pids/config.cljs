d4dd5989d0b02f7ac6ade6ccb919210602d83901
Alex Walker <alex@realeyes.com>
2018-04-04 09:08:51 -0600

    separated into poller file

--- src/pids/config.cljs
+++ src/pids/config.cljs
@@ -1,6 +1,7 @@
 (ns pids.config)
   (def streamtype "live")
   (def pidrange (into [] (concat ;(range 28500 31500)
-                        ;(range 200000 205500)
+                        (range 200000 205500)
                         ;(range 202700 204000)
-                        (range 200000 200100))))
+                        (range 206000 206100))))
+
--- src/pids/core.cljs
+++ src/pids/core.cljs
@@ -1,38 +1,9 @@
-(ns pids.core (:require-macros [cljs.core.async.macros :refer [go go-loop]])
-    (:require [rum.core :as rum]
-              [pids.config :as config]
-              [pids.parser :as parser]
-              [pids.reqs :as reqs]
-              [clojure.string :as cljstr]
-              [cljs.core.async :refer [<! timeout]]))
+(ns pids.core (:require [rum.core :as rum]
+                [pids.config :as config]
+                [pids.reqs :as reqs]
+                [pids.poller :as poller]
+                [clojure.string :as cljstr]))
 (enable-console-print!)
-
-(defn randstr [length]
-  (let [possible "abcdefghijklmnopqrstuvwxyz"]
-    (loop [accStr ""] (if (< (count accStr) length)
-      (recur (str accStr (nth possible (rand-int (count possible))))) accStr))))
-                   
-(defn setAll [dataAtom statsAtom filterParam]
-  (let [allPids config/pidrange
-         startTime (.now js/performance)]
-   (go-loop [pidsArr allPids]
-       (let [currentPid (first pidsArr)
-             status (:status (<! (reqs/getNbcJson currentPid "event_status_")))
-             sourceUrl (if (= filterParam status)
-               (:sourceUrl (first (:videoSources (<! (reqs/getNbcJson currentPid "live_sources_"))))) nil)
-             unauthCheck (if sourceUrl (<! (reqs/getResponse sourceUrl)))
-             setMResp (if (= (:status unauthCheck) 403) (<! (reqs/getResponse sourceUrl true)) unauthCheck)
-             streamUrl (if (= (:status setMResp) 200)
-                (parser/constructUrl sourceUrl (parser/getStream (cljstr/split-lines (:body setMResp)))))
-             isLive (if (not-empty streamUrl) (parser/isLive (:body (<! (reqs/getResponse streamUrl)))))]
-         (reset! statsAtom (str (* 100 (/ (- (count allPids) (count (rest pidsArr))) (count allPids))) "%"))
-       (if (not-empty sourceUrl)
-         (swap! dataAtom conj
-                {:pid currentPid :url sourceUrl :setMRespStatus (:status setMResp)
-                 :unauth (= (:status unauthCheck) 200) :isLive isLive}))
-       (if (not-empty pidsArr) (recur (rest pidsArr))
-           (reset! statsAtom (str "Found " (count @dataAtom) " of " (count allPids) " pids in "
-                (/ (- (.now js/performance) startTime) 1000) " seconds")))))))
 
 (rum/defc progressBar < rum/reactive [percentStr]
   [:div {:style {:width "700px" :border "1px solid black"}}
@@ -53,9 +24,9 @@
   (let [dataAtom (atom [])
         statsAtom (atom nil)]
      [:div {:style {:width "100%" :height "100%"}}
-     [:button {:on-click #(setAll dataAtom statsAtom config/streamtype)} "get"]
+     [:button {:on-click #(poller/startPoll dataAtom statsAtom config/streamtype)} "get"]
       [:button {:on-click #(reset! dataAtom [])} "clear"]
-      [:span  {:style {:font-size "11px"}} (str " using token " reqs/toke)]
+      [:span  {:style {:font-size "11px"}} (str " token " reqs/toke)]
      (progressBar statsAtom)
      (linesDisp dataAtom)]))
 
